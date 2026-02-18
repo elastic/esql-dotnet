edc2fcfa1d4d0cea6e7bfaf4ea985b888934e8f1
Martijn Laarman <Mpdreamz@gmail.com>
2026-02-18 18:33:30 +0100

    Fix build

--- examples/Elastic.Examples.Ingest/Ingestors/Strategies/IndexIngestStrategy.cs
+++ examples/Elastic.Examples.Ingest/Ingestors/Strategies/IndexIngestStrategy.cs
@@ -2,6 +2,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using Elastic.Channels;
 using Elastic.Clients.Elasticsearch;
