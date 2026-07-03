4ac617901abcd8cddebbb88975dc5190972aa4a3
Florian Bernd <florian.bernd@elastic.co>
2026-07-03 10:36:06 +0200

    Format aggregation constants with invariant culture

--- tests/Elastic.Esql.Tests/Translation/Aggregation/PercentileTests.cs
+++ tests/Elastic.Esql.Tests/Translation/Aggregation/PercentileTests.cs
@@ -2,10 +2,42 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System.Globalization;
+
 namespace Elastic.Esql.Tests.Translation.Aggregation;
 
 public class PercentileTests : EsqlTestBase
 {
+	[Test]
+	public void Percentile_FractionalValue_FormatsWithInvariantCulture()
+	{
+		var originalCulture = CultureInfo.CurrentCulture;
+		CultureInfo.CurrentCulture = new CultureInfo("de-DE");
+
+		try
+		{
+			var esql = CreateQuery<LogEntry>()
+				.From("logs-*")
+				.GroupBy(l => l.Level.MultiField("keyword"))
+				.Select(g => new
+				{
+					Level = g.Key,
+					P999 = EsqlFunctions.Percentile(g, l => l.Duration, 99.9)
+				})
+				.ToString();
+
+			_ = esql.Should().Be(
+				"""
+				FROM logs-*
+				| STATS p999 = PERCENTILE(duration, 99.9) BY level = log.level.keyword
+				""".NativeLineEndings());
+		}
+		finally
+		{
+			CultureInfo.CurrentCulture = originalCulture;
+		}
+	}
+
 	[Test]
 	public void Percentile_InGroupBy_GeneratesCorrectEsql()
 	{
--- tests/Elastic.Esql.Tests/Translation/Aggregation/TopValuesTests.cs
+++ tests/Elastic.Esql.Tests/Translation/Aggregation/TopValuesTests.cs
@@ -7,6 +7,26 @@ namespace Elastic.Esql.Tests.Translation.Aggregation;
 public class TopValuesTests : EsqlTestBase
 {
 	[Test]
+	public void Top_InGroupBy_GeneratesCorrectEsql()
+	{
+		var esql = CreateQuery<LogEntry>()
+			.From("logs-*")
+			.GroupBy(l => l.Level.MultiField("keyword"))
+			.Select(g => new
+			{
+				Level = g.Key,
+				TopDurations = EsqlFunctions.Top(g, l => l.Duration, 3, "desc")
+			})
+			.ToString();
+
+		_ = esql.Should().Be(
+			"""
+			FROM logs-*
+			| STATS topDurations = TOP(duration, 3, "desc") BY level = log.level.keyword
+			""".NativeLineEndings());
+	}
+
+	[Test]
 	public void Values_InGroupBy_GeneratesCorrectEsql()
 	{
 		var esql = CreateQuery<LogEntry>()
